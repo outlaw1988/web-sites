7b9467837ad45e5cb20a8c67f7ba92ac340c3c7f
Kuba Zamojski <kuba.zamojski@gmail.com>
2018-04-21 11:53:09 +0200

    Filtering

--- webSites/webSitesApp/views.py
+++ webSites/webSitesApp/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Website, WebPage, WebsiteCategory
+from django.db.models import Q
 
 
 class Websites(ListView):
     model = Website
     template_name = "websites.html"
+
+    # def get_context_data(self, *, object_list=None, **kwargs):
+
+    def get_queryset(self, *args, **kwargs):
+        queryset_list = Website.objects.all()
+        query = self.request.GET.get("q")
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(category=query)).distinct()
+        return queryset_list
 
 
 class WebsitesDetailView(DetailView):
