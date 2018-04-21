13a05d5363e24b9007bf3d08ab5d989ac9d72dba
Kuba Zamojski <kuba.zamojski@gmail.com>
2018-04-21 16:55:37 +0200

    filtering and sorting work on

--- webSites/webSitesApp/urls.py
+++ webSites/webSitesApp/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.Websites.as_view(), name='websites'),
+    path('websites', views.Websites.as_view(), name='websites-no-cat'),
+    path('websites_filter/<str:category>', views.Websites.as_view(), name='websites-cat'),
+    path('websites_order/<str:ordering>', views.Websites.as_view(), name='website-ord'),
     path('website/<int:pk>', views.WebsitesDetailView.as_view(), name='website-detail'),
     path('create_website/', views.CreateWebsite.as_view(), name='create-website'),
     path('create_webpage/', views.CreateWebPage.as_view(), name='create-webpage'),
--- webSites/webSitesApp/views.py
+++ webSites/webSitesApp/views.py
@@ -2,21 +2,57 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Website, WebPage, WebsiteCategory
 from django.db.models import Q
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import viewsets
+from .forms import CategoryForm
 
+
+# class Websites(viewsets.ModelViewSet):
+#     queryset = Website.objects.all()
+#     filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+#     filter_fields = ('category')
+#     ordering = ('title')
 
 class Websites(ListView):
     model = Website
     template_name = "websites.html"
 
-    # def get_context_data(self, *, object_list=None, **kwargs):
+    def get_queryset(self):
+        if "category" in self.kwargs:
+            category = WebsiteCategory.objects.filter(name=self.kwargs['category'])[0]
+            return Website.objects.filter(category=category)
+        if "ordering" in self.kwargs:
+            return Website.objects.all().order_by(self.kwargs['ordering'])
+        else:
+            return Website.objects.all()
 
-    def get_queryset(self, *args, **kwargs):
-        queryset_list = Website.objects.all()
-        query = self.request.GET.get("q")
-        if query:
-            queryset_list = queryset_list.filter(
-                Q(category=query)).distinct()
-        return queryset_list
+
+# class Websites(ListView):
+#     model = Website
+#     template_name = "websites.html"
+#     filter_backends = [SearchFilter, OrderingFilter]
+#     search_fields = ['title', 'category']
+#
+#     # def get_context_data(self, **kwargs):
+#     #     website_list = Website.objects.all()
+#     #     categories = WebsiteCategory.objects.all()
+#     #     context = {
+#     #         'website_list': website_list,
+#     #         'categories': categories,
+#     #     }
+#     #     return context
+#
+#     def get_queryset(self, *args, **kwargs):
+#         queryset_list = Website.objects.all()
+#         query = self.request.GET.get("q")
+#         if query:
+#             queryset_list = queryset_list.filter(
+#                 Q(category=query)).distinct()
+#         return queryset_list
+#
+#     # def get_queryset(self):
+#     #     category = WebsiteCategory.objects.filter(id=1)[0]
+#     #     return Website.objects.filter(category=category)
 
 
 class WebsitesDetailView(DetailView):
